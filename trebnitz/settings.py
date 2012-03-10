f997e90100ffe827603c621312bce4098d391e0f
Marcin Gębala <gemboy103@gmail.com>
2012-03-10 21:38:03 +0100

    Wylaczona obsluga statycznych plikow -test css

--- trebnitz/settings.py
+++ trebnitz/settings.py
@@ -45,7 +45,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/marcin/djcode/trebnitz/trebnitz/static'
+MEDIA_ROOT = '/static'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -118,7 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+#    'django.contrib.staticfiles',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'trebnitz.projects'
--- trebnitz/urls.py
+++ trebnitz/urls.py
@@ -15,8 +15,8 @@ urlpatterns = patterns('',
 
      url(r'^admin/', include(admin.site.urls)),
      url(r'^news/', news),
-     url(r'^static/(?P<path>.*)$', 
-         'django.views.static.serve', 
-         {'document_root': settings.MEDIA_ROOT},
-         {'show_indexes': True}),
+#     url(r'^static/(?P<path>.*)$', 
+#         'django.views.static.serve', 
+#         {'document_root': settings.MEDIA_ROOT},
+#         {'show_indexes': True}),
 )
